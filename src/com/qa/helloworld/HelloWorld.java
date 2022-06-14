b92099aa6f590948b6a3f55c531fe963e7f73317
wolottoh <ibuej@yahoo.co.uk>
2022-06-14 11:06:15 +0100

    first commit

---
+++ src/com/qa/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.qa.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+//		println is used to print text in a new line
+		
+//		DataType variableName = VALUE
+		
+		String message = "HelloWorld";
+		System.out.println(message);
+//	print is used to print text on the same line
+//	System.out.print("HelloWorld");
+	}
+}
